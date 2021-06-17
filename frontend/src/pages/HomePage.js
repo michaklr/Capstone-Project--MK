80172fb075af4505fd1e0254d44118c982dcbfea
Micha <Micha.amkessler@gmail.com>
2021-06-17 13:02:48 +0200

    Change Homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,6 +6,8 @@ import HeadlineUnderline from '../common/HeadlineUnderline'
 import { ReactComponent as RandomLogo } from '../img/Mental.svg'
 import Collapse from "../common/collapse";
 import {Link} from "react-router-dom";
+import HomepageCard from "../common/HomepageCard";
+import BulletList from "../common/BulletList";
 
 
 
@@ -28,6 +30,46 @@ export default function HomePage() {
                 </p>
             </Collapse>
 
+<CardContainer>
+    <HomepageCard>
+        <BulletList>
+            <li>Entdecken Sie Ihre individuellen Persönlichkeitsstile.</li>
+            <li>
+                Entwickeln Sie ein besseres Verständnis für sich selbst in
+                zwischenmenschlichen Beziehungen.
+            </li>
+            <li>
+                Profitieren Sie von speziell auf Sie zugeschnittenen Tipps für
+                Alltagssituationen.
+            </li>
+        </BulletList>
+
+    </HomepageCard>
+
+</CardContainer>
+<CardContainer>
+            <HomepageCard>
+                <li>
+                    <h3>Seriös</h3>
+                    <p>
+                        Unser Test basiert auf neuesten Forschungsergebnissen der
+                        Psychologie.
+                    </p>
+                </li>
+                <li>
+                    <h3>Schnell</h3>
+                    <p>Der Test dauert nur ca. 5 Minuten.</p>
+                </li>
+                <li>
+                    <h3>Sicher</h3>
+                    <p>
+                        Wir übermitteln keine Daten von Ihnen. Ihre Privatsphäre ist absolut
+                        sicher.
+                    </p>
+                </li>
+            </HomepageCard>
+</CardContainer>
+
 <button><Link to={'/manual'}>Start Test</Link></button>
         </Page>
     )
@@ -39,3 +81,36 @@ const LogoStyled = styled(RandomLogo)`
   max-width: 300px;
   margin-bottom:40px;
   `
+const CardContainer = styled.section`
+  background-color: #CCE3DE
+  border-radius: 0;
+  padding: 25px 15px;
+  
+  ul {
+    margin-bottom: 0;
+  }
+  
+  li {
+    list-style: none;
+    padding-left: 24px;
+    position: relative;
+    color: rgba(0,0,0,0.7);
+    
+    &:last-of-type {
+        margin-bottom: 0;
+    }
+    
+    &:before {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: #A4C3B2;
+        content:"";
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 2px;
+    }
+  }
+  
+`
