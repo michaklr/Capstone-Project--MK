abf2d64a9bc5e3dc88c8e6f90c027146bfd9b175
Micha <Micha.amkessler@gmail.com>
2021-06-25 12:47:41 +0200

    Returning the evaluated answers from backend

---
+++ backend/src/main/java/de/michaklr/backend/Dto/AnswersDto.java
@@ -0,0 +1,41 @@
+package de.michaklr.backend.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AnswersDto {
+    private String answer_0;
+    private String answer_1;
+    private String answer_2;
+    private String answer_3;
+    private String answer_4;
+    private String answer_5;
+    private String answer_6;
+    private String answer_7;
+    private String answer_8;
+    private String answer_9;
+    private String answer_10;
+    private String answer_11;
+    private String answer_12;
+    private String answer_13;
+    private String answer_14;
+    private String answer_15;
+    private String answer_16;
+    private String answer_17;
+    private String answer_18;
+    private String answer_19;
+    private String answer_20;
+    private String answer_21;
+    private String answer_22;
+    private String answer_23;
+    private String answer_24;
+    private String answer_25;
+    private String answer_26;
+    private String answer_27;
+    private String answer_28;
+
+}
